0a7ae153111156b8c35677a04defc74c634396b7
unknown <alen.nhk@gmail.com>
2019-11-03 21:16:58 +0530

    setup class

--- manage_relation/tests.py
+++ manage_relation/tests.py
@@ -3,6 +3,10 @@ from django.urls import reverse
 
 
 class IndexViewTest(TestCase):
+    def setUp(self):
+        import django
+        django.setup()
+
     def test_simple_index(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)

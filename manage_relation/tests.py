a2fc908dfaab82b653700c681e20463766ebf165
unknown <alen.nhk@gmail.com>
2019-11-03 16:54:29 +0530

    Simple unit test

--- manage_relation/tests.py
+++ manage_relation/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTest(TestCase):
+    def test_simple_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)

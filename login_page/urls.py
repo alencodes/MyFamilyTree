1760f54abd9a5f633865de1c8176d76877065e78
Nishabu Hameed K <NishabuK1@verifone.com>
2019-11-07 19:46:53 +0530

    Sample login and register

--- login_page/urls.py
+++ login_page/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from login_page import views
 
 urlpatterns = [
-    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/', views.register, name='register'),
 ]
